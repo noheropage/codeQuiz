dee1a8cf5583add67997b1d2cf0fca3e41712e7a
noheropage <mursepage@gmail.com>
2021-01-14 18:37:31 -0800

    Scaffolding and basic structure

---
+++ script.js
@@ -0,0 +1,9 @@
+// TODO: timer displayed (upper right hand corner perhaps) 
+
+// TODO: start button 
+
+// TODO: generate question with multiple answers 
+
+// TODO: end when timer hits 0 or run outta Qs 
+
+// TODO: save score via initials 
