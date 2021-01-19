79028e58522bcb1dae3d2a651ba860c2995c2d9d
noheropage <mursepage@gmail.com>
2021-01-19 00:15:49 -0800

    Add functions for start button and generating first question

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 var timeEl = document.querySelector(".time");
+var startButton = document.querySelector("#start")
+var questionEl = document.querySelector(".quiz");
+var askEl = document.querySelector(".ask");
+var answerEl = document.querySelectorAll(".answers");
+var startEl = document.querySelector(".starter");
+
 var secondsLeft = 10;
+var score = 0;
 
-setTime();
+var questions = [
+    {
+        ask: "What is your favorite color?",
+        answer: ["blue", "red", "yellow", "green"],
+        correct: "red",
+    },
 
-
+]
 
 // TODO: timer displayed (upper right hand corner perhaps) 
 function setTime() {
-    var timerInterval = setInterval(function() {
+    var timerInterval = setInterval(function () {
         secondsLeft--;
         timeEl.textContent = secondsLeft
 
@@ -18,10 +30,45 @@ function setTime() {
         }
     }, 1000);
 }
+
+// TODO: remove time for wrong answers 
+function wrongAnswer() {
+    // remove time 
+    // display message 
+    // move to next question 
+}
+
+function rightAnswer() {
+    // display message 
+    // move to next question 
+}
 // TODO: start button 
+startButton.addEventListener("click", function () {
+    setTime();
+    // Hides starting text 
+    startEl.style.visibility = "hidden";
+    generateQuestion();
+    // console.log(answerEl[1].value)
+})
 
 // TODO: generate question with multiple answers 
+function generateQuestion() {
+    // questionEl.style.visibility = "visible";
+    // questionEl.textContent = "Is this working?"
+    for (var i=0; i < questions.length; i++) {
+        askEl.textContent = questions[i].ask;
+        for (var n=0; n < questions[i].answer.length; n++){
+            console.log(answerEl[n].textContent)
+            console.log(questions[i].answer[n])
+            answerEl[n].textContent = questions[i].answer[n]
+            console.log(answerEl[n].textContent)
+        }  
+    }
+}
 
+function multipleChoice () {
+
+}
 // TODO: end when timer hits 0 or run outta Qs 
 
 // TODO: save score via initials 
