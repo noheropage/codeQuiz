ac7650f83786c5fcb763771492a00c81538a1a6c
noheropage <mursepage@gmail.com>
2021-01-18 21:19:13 -0800

    Establish basic timer function and grid placement

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-// TODO: timer displayed (upper right hand corner perhaps) 
+var timeEl = document.querySelector(".time");
+var secondsLeft = 10;
 
+setTime();
+
+
+
+// TODO: timer displayed (upper right hand corner perhaps) 
+function setTime() {
+    var timerInterval = setInterval(function() {
+        secondsLeft--;
+        timeEl.textContent = secondsLeft
+
+        if (secondsLeft <= 0) {
+            clearInterval(timerInterval);
+            // TODO: 
+            // timesOut();
+        }
+    }, 1000);
+}
 // TODO: start button 
 
 // TODO: generate question with multiple answers 
